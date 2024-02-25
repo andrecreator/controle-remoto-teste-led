02cbfb085f6059b5a3aa878474e18b8805c52633
andrecreator <107895748+andrecreator@users.noreply.github.com>
2024-02-25 15:42:14 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,45 @@
+function car_back () {
+    mecanumRobot.Motor(LR.Upper_left, MD.Back, 75)
+    mecanumRobot.Motor(LR.Lower_left, MD.Back, 75)
+    mecanumRobot.Motor(LR.Upper_right, MD.Back, 75)
+    mecanumRobot.Motor(LR.Lower_right, MD.Back, 75)
+}
+function car_left () {
+    mecanumRobot.Motor(LR.Upper_left, MD.Back, 75)
+    mecanumRobot.Motor(LR.Lower_left, MD.Back, 75)
+    mecanumRobot.Motor(LR.Upper_right, MD.Forward, 75)
+    mecanumRobot.Motor(LR.Lower_right, MD.Forward, 75)
+}
+function car_forward () {
+    mecanumRobot.Motor(LR.Upper_left, MD.Forward, 75)
+    mecanumRobot.Motor(LR.Lower_left, MD.Forward, 75)
+    mecanumRobot.Motor(LR.Upper_right, MD.Forward, 75)
+    mecanumRobot.Motor(LR.Lower_right, MD.Forward, 75)
+}
+function car_right () {
+    mecanumRobot.Motor(LR.Upper_left, MD.Forward, 75)
+    mecanumRobot.Motor(LR.Lower_left, MD.Forward, 75)
+    mecanumRobot.Motor(LR.Upper_right, MD.Back, 75)
+    mecanumRobot.Motor(LR.Lower_right, MD.Back, 75)
+}
+basic.showIcon(IconNames.Sword)
+irRemote.connectInfrared(DigitalPin.P9)
+let val = 0
+let val2 = 0
+basic.forever(function () {
+    val = irRemote.returnIrButton()
+    if (val != 0) {
+        val2 = val
+        if (val2 == 70) {
+            car_forward()
+        } else if (val2 == 68) {
+            car_left()
+        } else if (val2 == 67) {
+            car_right()
+        } else if (val2 == 21) {
+            car_back()
+        } else if (val2 == 64) {
+            mecanumRobot.state(MotorState.stop)
+        }
+    }
+})
